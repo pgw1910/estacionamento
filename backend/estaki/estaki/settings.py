728fe886d062f260b3992e2ec8e4e6b84ba1be32
pgw1910 <pedroribeiroegj@gmail.com>
2026-06-02 19:13:21 -0300

    inicio da api e criação das funçoes e classes

--- backend/estaki/estaki/settings.py
+++ backend/estaki/estaki/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'park'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files (imagens das placas)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Token do Plate Recognizer
+PLATE_RECOGNIZER_TOKEN = '702aaa3277bfde663985695c2d603b9600bf4daf'
